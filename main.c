bc3046f6ba7e8b9e449d4cd8e8cb8ee7090ac742
Moreau Cyril <spl3en.contact@gmail.com>
2014-02-19 17:50:45 +0100

    Update the patchnotes and the version

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include "./Crypto/md5.h"
 #include <signal.h>
 
-#define LOLCAMERA_VERSION 0.210
+#define LOLCAMERA_VERSION 0.211
 
 char download_link[] = "https://sourceforge.net/projects/lolcamera/files";
 char download_host[] = "cznic.dl.sourceforge.net";
