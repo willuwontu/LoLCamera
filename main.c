d9b2e2dd27d7866b8c4190cb615c1338d12aeb99
Spl3en <spl3en.contact@gmail.com>
2014-04-05 22:55:38 +0200

    Update version.

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include "./Crypto/md5.h"
 #include <signal.h>
 
-#define LOLCAMERA_VERSION 0.232
+#define LOLCAMERA_VERSION 0.233
 
 char download_link[] = "https://sourceforge.net/projects/lolcamera/files";
 char download_host[] = "cznic.dl.sourceforge.net";
